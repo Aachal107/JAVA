2ac7a83383cd0da289f963056ceae2a9d5383d5f
Aachal107 <singhaachal103@gmail.com>
2023-06-21 22:34:38 +0530

    initial commit

---
+++ array/Reverse.java
@@ -0,0 +1,19 @@
+// package array;
+import java.util.Scanner; 
+public class Reverse {
+    public static void main (String args[])
+    {
+        //Scanner in = new Scanner(System.in);
+        int num = 12345;
+        int ans=0;
+        while(num>0)
+        {
+            int rem = num%10;
+            num=num/10;
+            ans=ans*10+rem;
+        }
+        System.out.println("reverse:"+ans);
+
+
+    }
+}
